f9b3835423612919cd0b6d67860fac0f95e6037c
Boris Valero <Boris Valero>
2025-09-19 12:12:20 +0200

    correction HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -21,7 +21,6 @@ const sadSong = ref(null)
 
 async function handleSubmit() {
   if (answer.value.trim().toLowerCase().includes('triste')) {
-    // Appelle l’API pour une chanson triste
     const res = await fetch('http://localhost:3000/api/sad-song')
     if (res.ok) {
       sadSong.value = await res.json()
